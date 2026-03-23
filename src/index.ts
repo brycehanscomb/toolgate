6153640e0c4c585d5371f7ec80cac527b7520bac
Bryce <bryce@brycehanscomb.com>
2026-03-23 20:42:08 +1100

    fix: export runPolicy from barrel

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { ALLOW, DENY, NEXT, allow, deny, next, isVerdictResult } from './verdicts'
 export type { ToolCall, CallContext, VerdictResult, Middleware } from './types'
-export { definePolicy } from './policy'
+export { definePolicy, runPolicy } from './policy'
